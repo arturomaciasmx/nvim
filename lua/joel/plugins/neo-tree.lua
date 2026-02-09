a09bb037de81791080730fb6a5809b3b17703f2f
Arturo <arturomaciasmx@gmail.com>
2026-02-09 09:07:52 -0800

    fix neo tree and change keymap <leader>e

--- lua/joel/plugins/neo-tree.lua
+++ lua/joel/plugins/neo-tree.lua
@@ -7,12 +7,66 @@ return {
       "MunifTanjim/nui.nvim",
     },
     cmd = "Neotree",
-    -- ADD THIS SECTION
+    keys = {
+      {
+        "<leader>e",
+        function()
+          -- Ensure your custom LazyVim.root() function from earlier is defined!
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+      -- because `cwd` is not set up properly.
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+        desc = "Start Neo-tree with directory",
+        once = true,
+        callback = function()
+          if package.loaded["neo-tree"] then
+            return
+          else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+              require("neo-tree")
+            end
+          end
+        end,
+      })
+    end,
     opts = {
       filesystem = {
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
       },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["<space>"] = "none",
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["O"] = {
+            function(state)
+              require("lazy.util").open(state.tree:get_node().path, { system = true })
+            end,
+            desc = "Open with System Application",
+          },
+          ["P"] = { "toggle_preview", config = { use_float = false } },
+        },
+      },
+
       default_component_configs = {
         icon = {
           folder_closed = "",
@@ -25,21 +79,32 @@ return {
           symbols = {
             unstaged = "○",
             staged = "●",
-             untracked = ""
+            untracked = ""
           },
         },
       },
     },
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          -- Ensure your custom LazyVim.root() function from earlier is defined!
-          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+    config = function(_, opts)
+      local function on_move(data)
+        Snacks.rename.on_rename_file(data.source, data.destination)
+      end
+
+      local events = require("neo-tree.events")
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED,   handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
         end,
-        desc = "Explorer NeoTree (Root Dir)",
-      },
-    },
+      })
+    end,
+
   }
 }
-
