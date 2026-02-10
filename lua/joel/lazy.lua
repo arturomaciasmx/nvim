f6891ea2eb2d4a018ac19a276119e4e76bfd5d36
Joel <joel@Joels-MacBook.local>
2026-02-09 21:58:03 -0800

    fix installation

--- lua/joel/lazy.lua
+++ lua/joel/lazy.lua
@@ -1,30 +1,36 @@
 -- 1. Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com", "--branch=stable", lazypath })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- 2. Setup plugins
 require("lazy").setup({
-  spec = {
-    -- LOAD THE ENGINE ONLY (provides the 'LazyVim' global variable)
-    {
-      "LazyVim/LazyVim",
-      priority = 10000,
-      lazy = false,
-      config = true,
-      opts = {
-        defaults = {
-          autocmds = true, -- Set to false if you want zero defaults
-          keymaps = false, -- THIS FIXES YOUR ERROR
-          options = true,
-        },
-      }
-    },
+	spec = {
+		-- LOAD THE ENGINE ONLY (provides the 'LazyVim' global variable)
+		{
+			"LazyVim/LazyVim",
+			priority = 10000,
+			lazy = false,
+			config = true,
+			opts = {
+				defaults = {
+					autocmds = true, -- Set to false if you want zero defaults
+					keymaps = false,
+					options = true,
+				},
+			},
+		},
 
-    -- IMPORT YOUR STUFF (from lua/plugins/ folder)
-    { import = "joel.plugins" },
-  },
+		-- IMPORT YOUR STUFF (from lua/plugins/ folder)
+		{ import = "joel.plugins" },
+	},
 })
-vim.treesitter.language.register("liquid", "liquid")
