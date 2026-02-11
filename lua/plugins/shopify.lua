9095d3ec4e36ff2c74ac33d062452acd5e30334c
Arturo Macias <arturomaciasmx@gmail.com>
2026-02-11 08:31:16 -0800

    Fix shopify suggestions

--- lua/plugins/shopify.lua
+++ lua/plugins/shopify.lua
@@ -1,27 +1,10 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        shopify_theme_ls = {
-          cmd = { "shopify", "theme", "language-server", "--stdio" },
-          filetypes = { "liquid" },
-        },
-        cssls = {
-          -- This forces the CSS LSP to initialize on liquid files
-          filetypes = { "css", "scss", "less", "liquid" },
-          settings = {
-            css = { validate = true },
-          },
-        },
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        liquid = { "prettier" },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      shopify_theme_ls = {
+        -- Ensure blink capabilities are merged
+        capabilities = require("blink.cmp").get_lsp_capabilities(),
       },
     },
   },
